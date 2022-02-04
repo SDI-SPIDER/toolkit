c9787cee0e8c10858727cc462c5f6746822096b1
Mark Panek <mark.panek@hs-bochum.de>
2022-02-04 11:23:58 +0100

    Revert "Add Link to BoK to LO's"
    This reverts commit ed993cefecf083e501ba558ca73a5d45d32a4f86.

--- spider.js
+++ spider.js
@@ -183,16 +183,6 @@ function formatMaterials(materials) {
     `);
 }
 
-/* Add a list of Bodies of Knowlege Links*/
-function addBokList(bok) {
-	list = `<div class="bok">`;
-	bok.foreach(function(bokItem) {
-		list.append(`<a href="` + bokItem["URL"] + `" alt="` + bokItem["Topic"] + `" target="_blank">` + bokItem["Source"] + `</a>`)
-	});
-	list.append(`</div>`);
-	return list;
-}
-
 /* Remove blanks and punctuation from a string so we can use it as ID for a node in the HTML tree */
 function id(st) {
   return st.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
@@ -289,11 +279,6 @@ $.getJSON("toolkit.json", function(data) {
                   function(l, assessment) {
                     lopbc.append(formatAssessment(assessment))
                   });
-				  
-				// BoK to the body content, if existing:
-				if (learningOutcome["BoK"]) {
-					lopbc.append(addBokList(learningOutcome["BoK"]))
-				}
 
                 // … panel body content to panel body
                 lopb = formatLOPanelBody(learningOutcome)
